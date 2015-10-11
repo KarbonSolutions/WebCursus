917a12c64008f769a83d454a9fe4c0bbf2fc38e3
Joshua Bonsink <joshua@karbonsolutions.nl>
2015-10-11 20:28:10 +0200

    Javascript assignments

---
+++ Javascript/Opdracht 2/Maher/Tic Tac Toe/app.js
@@ -0,0 +1,14 @@
+﻿// Boter, kaas en eieren
+
+function ticTacToe() {    
+    // Registreer hier onClick events voor de negen knopjes:
+    // 
+    // Tips:
+    // Voeg de css class 'o' toe om een O te tonen. Evt. ook 'btn-primary'.
+    // Voeg de css class 'x' toe om een X te tonen. Evt. ook 'btn-info'.
+    // Vergeet niet te controleren of er al eerder is geklikt op de button. Kijk naar de aanwezigheid van css class 'o' of 'x'.
+    // Houd een interne administratie bij om te bepalen of er iemand heeft gewonnen.
+
+    //Registreer hier de onClick event voor de reset knop    
+};
+
