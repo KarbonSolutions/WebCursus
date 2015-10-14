c8dbd5ebbe7f7620f5ffcd311b6b58d2a02d4650
meijerj <meijerj@baxter.com>
2015-10-14 17:00:36 +0200

    Store

---
+++ dotnet/Johan/Store/Store/Controllers/Context.cs
@@ -0,0 +1,16 @@
+﻿using Store.Models; // toevoegen via suggestions fix
+using System;
+using System.Collections.Generic;
+using System.Data.Entity; // toevoegen
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store.Controllers
+{
+    public class Context : DbContext 
+    {
+        public Context() : base("Store") { }
+        public DbSet <Album> Albums { get; set; }
+    }
+}
---
+++ dotnet/Johan/Store/Store/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store.Models
+{
+    [Table("Albums")]
+    public class Album
+    {
+        public int ID { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(50)]
+        public string Artist { get; set; }
+        public double Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
