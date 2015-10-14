5d28aa80d06d84f8f2e5267d2701f7da16230069
Muzzi11 <muzzi11@hotmail.com>
2015-10-14 07:45:15 +0200

    dotnet

---
+++ dotnet/Mustafa/Music Store/Music Store/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Music_Store.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("MusicStore")
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+    }
+}
