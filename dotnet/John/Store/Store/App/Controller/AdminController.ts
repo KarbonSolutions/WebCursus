3d63cd5b7d446b8c7823618a7f611678dbe4d8a9
jsilooy <john.silooy@yoolis.nl>
2015-10-26 15:05:52 +0000

    Colors added

---
+++ dotnet/John/Store/Store/App/Controller/AdminController.ts
@@ -0,0 +1,18 @@
+﻿
+angular.module('store').controller('AdminController',
+    function ($scope, $http) {
+
+        $scope.products = [];
+
+        $scope.addAlbum = function () {
+            alert('test')
+            //$scope.products.push({
+            //    title: albums[i].Title,
+            //    artist: albums[i].Artist,
+            //    price: albums[i].Price,
+            //    releaseDate: new Date(albums[i].ReleaseDate),
+            //});
+
+        };
+
+    });
